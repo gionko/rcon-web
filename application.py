6789d5976a6dcf44d8aacfa56d65f2372f84e4f1
Andrejs Cainikovs <andrejs.cainikovs@gmail.com>
2017-03-24 16:04:31 +0000

    Implement frontend

--- application.py
+++ application.py
@@ -6,6 +6,12 @@ import simplekv.memory
 # Create Flask application
 app = flask.Flask(__name__)
 
+# Configure application
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY='diebitch'
+)
+
 # Create session store
 store = simplekv.memory.DictStore()
 flask_kvsession.KVSessionExtension(store, app)
