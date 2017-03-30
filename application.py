c74fa242a7d075ae766259a7a4eb5d9109e53988
Andrejs Cainikovs <andrejs.cainikovs@gmail.com>
2017-03-30 13:16:20 +0000

    Add configuration file, pass data to template

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
+import configobj
 import flask
 import flask_kvsession
+import os
 import simplekv.memory
 
 
@@ -16,7 +18,30 @@ app.config.update(
 store = simplekv.memory.DictStore()
 flask_kvsession.KVSessionExtension(store, app)
 
+# Load configuration
+__path__ = os.path.dirname(os.path.realpath(__file__))
+config = configobj.ConfigObj(__path__ + '/rcon-web.conf')
+
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    players_status = '2 humans, 8 bots'
+    players = [
+        {
+            'name': 'Player 1',
+            'value': 'STEAM_1:1:12345678'
+        },
+        {
+            'name': 'Player 2',
+            'value': 'STEAM_1:1:87654321'
+        }
+    ]
+
+    maps_status = 'buhriz_coop'
+    maps = [{'name': name, 'value': config['maps'][name]['value']} for name in config['maps']]
+
+    return flask.render_template('index.html',
+                                 players_status=players_status,
+                                 players=players,
+                                 maps_status=maps_status,
+                                 maps=maps)
